a5599feba68374ec804c91a9b3dcf291419ce753
Pushparaj13811 <hmehta588@rku.ac.in>
2024-03-12 09:52:29 +0530

    Created a counter app using react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+  let [message, setMessage] = useState('')
+
+  const addValue = () => {
+    if (counter < 20) {
+      setMessage(' ')
+      setCounter(counter + 1)// here we can't pass counter++. It will create a problem  in the next render cycle.
+    } else {
+      message = `You can't  add more than 20!`
+      setMessage(message)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setMessage(' ')
+      setCounter(counter - 1) // here we can't pass counter--. It will create a problem  in the next render cycle.
+    } else {
+      message = `You can't go beyond 0`
+      setMessage(message)
+    }
+  }
+  return (
+    <>
+      <h1>Pushparaj Counter APP</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value {counter}</button>
+      <button onClick={removeValue}>remove value {counter}</button>
+      <p>{counter}</p>
+      <p>{message}</p>
+    </>
+  )
+}
+
+export default App
