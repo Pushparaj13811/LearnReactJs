dc3f034eb812dba4f7de74d9c889ddc6e6771a35
Pushparaj13811 <hmehta588@rku.ac.in>
2024-03-15 09:46:42 +0530

    Added the currency rate showing functionality

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -33,14 +33,18 @@ function App() {
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
   }
-  const BackgroundImage = `https://images.pexels.com/photos/220887/pexels-photo-220887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`
+  
+  const BackgroundImage = "../src/assets/background-image.jpg"
   return (
     <div
-      className="w-full h-screen flex flex-wrap bg-cover  bg-no-repeat"
+      className="w-full lg:h-screen flex flex-wrap lg: bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('${BackgroundImage}')`,
       }}
     >
+      <div className='w-full text-center text-2xl md:text-3xl lg:text-3xl  max-w-md md:mx-auto border lg:-mb-20 mt-5 mx-5 border-gray-60 h-20 rounded-lg p-5 backdrop-blur-sm bg-white/30 '>
+        Currency Converter
+      </div>
       <div className="w-full lg:flex lg:justify-evenly mt-5 ml-5 mr-5">
         <div className="w-full max-w-md md:mx-auto border mb-5 border-gray-60 h-72 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
@@ -83,6 +87,25 @@ function App() {
               Convert {from} to {to}
             </button>
           </form>
+        </div>
+        <div className='w-full  max-w-md md:mx-auto border mb-5 border-gray-60 h-72 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+          <h1 className='text-center text-xl pb-3'>Currency Conversion rate</h1>
+          <table className='text-center border-collapse border border-gray-600 mx-auto'>
+            <thead >
+              <tr className='border-b border-gray-600'>
+                <th className='border-r border-gray-600 px-4 py-2'>Currency</th>
+                <th className='px-4 py-2'>Rate</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(currencyData).map(([currency, rate]) => (
+                <tr className='border-b border-gray-600' key={currency}>
+                  <td className='border-r border-gray-600 px-4 py-2'>{currency}</td>
+                  <td className='px-4 py-2'>{rate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
