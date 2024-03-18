10366b724392aa40a2c15d38ea262476fe99fd14
Pushparaj13811 <hmehta588@rku.ac.in>
2024-03-18 21:43:26 +0530

    Updated App.jsx

--- backgroundchanger/src/App.jsx
+++ backgroundchanger/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   let [color, setColor] = useState('green')
   return (
     <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
-
       <div className=' fixed flex flex-wrap justify-center rounded-lg flex-column bottom-10  inset-x-0 p-2 '>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-gray-200 px-2 py-2 rounded-3xl'>
           <button onClick={() => setColor('red')} className='outline-none px-3 py-2 rounded-xl' style={{ backgroundColor: "red" }} >red</button>
