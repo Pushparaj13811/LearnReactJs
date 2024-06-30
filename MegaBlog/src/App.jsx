631db4dad667414f1ad9227ea866581236872502
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-30 12:16:55 +0530

    Updated app.jsx

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { } from "./store/authSlice"
+import Navbar from './components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        }
+        else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      {/* <Outlet /> */}
+    </>
   )
 }
 
